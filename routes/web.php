4989df937e302d7ae1889f974a2ec4a0572bba9d
Abdielkisumba <abdiel3microprocesseuri100@gmail.com>
2022-02-27 12:06:44 +0100

    contribution du microprocesseur

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+Use App\Http\Controllers\ProduitCotroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new_produit', function () {
+    return view('new_produit');
+})->name('new_produit');
+
+Route::get('/produit', [ProduitCotroller::class,'index'])->name('produit');
+Route::get('/edit_produit/{id}', [ProduitCotroller::class, 'edit'])->name('edit_produit');
+
+Route::post('/store_produit',[ProduitCotroller::class,'store'])->name('store_produit');
+Route::post('/update_produit',[ProduitCotroller::class,'update'])->name('update_produit');
+Route::get('/delete_produit/{id}',[ProduitCotroller::class,'destroy'])->name('delete_produit');
